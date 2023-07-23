dfc16ee78100f59c4071e609e3eb50eadb1bd4c2
tinari <kurisumasunama@yahoo.co.jp>
2023-07-23 13:30:11 +0900

    ログアウトユーザーの挙動修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,only:[:edit, :show]
+  before_action :authenticate_user!,only:[:edit]
   before_action :move_to_index, except: [:index, :show, :new, :create]
   
 
